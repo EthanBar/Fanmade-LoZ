6fefe120fe76ba879844813a08ce707c14b1b149
Reagan Russell <nitrodragon@users.noreply.github.com>
2017-06-16 08:03:27 -0700

    Update ZeldaPlayWindow.java
    Check the add/del. I commented a bunch and fixed some formatting. I love this heckin file.

--- src/ZeldaPlayWindow.java
+++ src/ZeldaPlayWindow.java
@@ -16,71 +16,70 @@
 
 public class ZeldaPlayWindow extends JFrame {
 
+	Link thePlayer; //The instance of Link
+    	map theMap; //The instance of the map in the background (not a minimap)
+	
 	public final int NORTH = 0;
 	public final int EAST = 1;
 	public final int SOUTH = 2;
 	public final int WEST = 3; //Numbers defining the compass directions, never used
+	
 	public int x, y; // Link's x and y positions, I wanna say?
 	public BufferedImage screenShot; //Nothing important
 	public Monster[] theMonster = new Monster[10]; // Array of Monsters
-	private String[] monString = new String[10]; // Monster IDs, who it is, etc.
 	public Random r = new Random(); // Idk
+	
+	private String[] monString = new String[10]; // Monster IDs, who it is, etc.
 	private BufferedReader br; // File-handling for tilesets
-    boolean intro = false; // For the intro, I'm setting it to false. Don't want no intro crap
-
-    Link thePlayer; //The instance of Link
-    map theMap; //The instance of the map in the background (not a minimap)
+	
+    	boolean intro = false; // For the intro, I'm setting it to false. Don't want no intro crap
     
 	public ZeldaPlayWindow(String title) throws Exception {
 		super(title);
-		this.setSize(544,400);
+		this.setSize(544, 400);
 		this.setResizable(false);
-		this.setLocationRelativeTo(null); // Get it in the center
-		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //Sets window commands
+		this.setLocationRelativeTo(null); // Get that in the center
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Sets window commands
 
 		BorderLayout border = new BorderLayout();
 		thePlayer = new Link (300, 200, this); // Creates an instance of Player as Link
 		this.add(thePlayer); // Actually attaches Link to the screen
 		theMap = new map (this);
-		//FlowLayout ItemPanel = new FlowLayout();  //Organizes the layout, sadly it doesn't exist
+		//FlowLayout ItemPanel = new FlowLayout();  // Organizes the layout, sadly it doesn't exist
 		this.setLayout(border);
 		this.add(theMap, BorderLayout.CENTER);
 
-		monLoad();
+		monLoad(); // That's a dumb name to load all the monsters.
 
-		this.setVisible(true); //Makes final adjustments
+		this.setVisible(true); // Makes this visible
 
-		try{
-			Thread.sleep(3000); //Delay his next image change
+		try {
+  	     	    Thread.sleep(3000); // Delay his next image change
 		}
 		catch (InterruptedException e){
-		    e.printStackTrace();
+		    e.printStackTrace(); // Prints the Stack Trace in case anything goes wrong. (It won't)
 		}
-
 		this.intro = false;
-
 	}
 
-    void monLoad() throws IOException{
+   	 void monLoad() throws IOException {
 		this.theMap.loading = true;
 		br = new BufferedReader(new FileReader("res/" + this.theMap.x + " " + this.theMap.y + ".txt")); //Reads from the file specified for the map
 
-		for (int i = 0; i < 362; i++){
-			if (i > 351){
-				monString[i-352] = br.readLine();
-			}
-			else {
-				br.readLine();
+		for (int i = 0; i < 362; i++) {
+			if (i > 351) {
+			    monString[i-352] = br.readLine();
+			} else {
+			    br.readLine();
 			}
 		}
 
 		for (int i = 0; i < 10; i++){
 			if (theMonster[i] == null){
 				do {
-					x = r.nextInt(14) + 1;
-					y = r.nextInt(9) + 1;
+				    x = r.nextInt(14) + 1;
+				    y = r.nextInt(9) + 1;
 				} while (this.theMap.walkable[(y*16) + x] != 1);
-
 				theMonster[i] = chooseMon(x, y, monString[i], i);
 				(new Thread(theMonster[i])).start();
 			}
@@ -90,8 +89,8 @@ public class ZeldaPlayWindow extends JFrame {
 
     private Monster chooseMon(int monX, int monY, String theMonString, int theNum) throws IOException{
 
-		switch (theMonString) {
-			case ("redoctorok"):
+		switch (theMonString) { // Takes the string of each monster and loads it hecka fast
+			case ("redoctorok"): // Screw if/else loops
 				return new RedOctorok(monX, monY, this, theNum);
 			case ("blueoctorok"):
 				return new BlueOctorok(monX, monY, this, theNum);
@@ -117,9 +116,8 @@ public class ZeldaPlayWindow extends JFrame {
 
     }
 
-    public static void main(String[] args) throws Exception {
-	    @SuppressWarnings("unused")
-        AtomicReference<ZeldaPlayWindow> theZeldaPlayWindow = new AtomicReference<>(new ZeldaPlayWindow("The Legend of Zelda"));
+    public static void main(String[] args) throws Exception { // 
+        AtomicReference<ZeldaPlayWindow> theZeldaPlayWindow = new AtomicReference<>(new ZeldaPlayWindow("The Legend of Zelda")); // I'm a fan of AtomicReferences
     }
 
 }
